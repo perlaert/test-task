05fc69f1150b94eea141f80f0054725cbec61911
Katerina Kucheriavenko <ekucheryavenko0925@gmail.com>
2021-10-15 14:29:44 +0300

    fixes mistakes

--- src/services/images-api.js
+++ src/services/images-api.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://pixabay.com/api";
 
-const fetchImages = () => {
+async function fetchImages() {
   const apiKey = "19974977-7de7da89e9a7910ce59988326";
-  return axios.get(`/?q=nature&page=1&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=8`).then((response) => response.data.hits);
-};
+  return await axios.get(`/?q=nature&page=1&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=8`).then((response) => response.data.hits);
+}
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default { fetchImages };
